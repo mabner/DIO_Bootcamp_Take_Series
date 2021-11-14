5b8f3991eb2e59d729e0b2dd527b585d0d95916d
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-11-14 18:13:47 -0300

    adds base entity and series class files

---
+++ Classes/Serie.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Series
+{
+	public class Serie : EntidadeBase
+	{
+		// Atributos
+		private Genero Genero { get; set; }
+		private string Titulo { get; set; }
+		private string Descricao { get; set; }
+		private int Ano { get; set; }
+
+		// Métodos
+		public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+		{
+			this.Id = id;
+			this.Genero = genero;
+			this.Titulo = titulo;
+			this.Descricao = descricao;
+			this.Ano = ano;
+		}
+	}
+}
