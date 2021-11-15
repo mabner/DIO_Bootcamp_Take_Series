49c7b978bfabf692a2a2c4756d894ecd30f7d639
Marcos Leite <30819867+mabner@users.noreply.github.com>
2021-11-15 00:56:47 -0300

    adds deleted? field to Serie.cs

--- Classes/Serie.cs
+++ Classes/Serie.cs
@@ -12,6 +12,7 @@ namespace Series
 		private string Titulo { get; set; }
 		private string Descricao { get; set; }
 		private int Ano { get; set; }
+		private bool Excluido { get; set; }
 
 		// Métodos
 		public Serie(int id, Genero genero, string titulo, string descricao, int ano)
@@ -21,27 +22,33 @@ namespace Series
 			this.Titulo = titulo;
 			this.Descricao = descricao;
 			this.Ano = ano;
+			this.Excluido = false;
 		}
 
 		public override string ToString()
 		{
 			string retorno = "";
-            // Emviroment.NewLine insere uma nova linha uvando a forma que o sistema operacional trabalha
+			// Emviroment.NewLine insere uma nova linha uvando a forma que o sistema operacional trabalha
 			retorno += "Gênero: " + this.Genero + Environment.NewLine;
 			retorno += "Título: " + this.Titulo + Environment.NewLine;
 			retorno += "Descrição: " + this.Descricao + Environment.NewLine;
 			retorno += "Ano de Início: " + this.Ano;
-            return retorno;
+			return retorno;
 		}
 
-        public string retornaTitulo()
-        {
-            return this.Titulo;
-        }
+		public string retornaTitulo()
+		{
+			return this.Titulo;
+		}
 
-        internal int retornaId()
-        {
-            return this.Id;
-        }
+		internal int retornaId()
+		{
+			return this.Id;
+		}
+
+		public void Excluir()
+		{
+			this.Excluido = true;
+		}
 	}
 }
